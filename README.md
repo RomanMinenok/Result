f98c7f352633c0032d59edb5cf5987d197600d16
greennick <paul.grynyk@gmail.com>
2019-09-13 16:04:49 +0300

    - prepare README;  - prepare docs;

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
+[![](https://jitpack.io/v/green-nick/Result.svg)](https://jitpack.io/#green-nick/Result)
 # Result
-Result holder library
+Small class and extensions that are used as some result holder.
+In fact this is Either monad implementation, but oriented on "results".
+Unlike Kotlin's default Result could be used without any compiler flags and error part could be any object.
+
+## Usage
+### Initialization:
+Representing success result:  
+`val success: Result<String, Nothing> = Result.success("hello")`
+
+Representing success result without return type:  
+`val success: Result<Unit, Nothing> = Result.success()`
+
+Representing error:  
+`val error: Result<Nothing, Throwable> = Result.error(IllegalStateException("wrong action))`
+
+try-catch replacement:  
+```
+val result: Result<Int, Throwable> = tryWithResult {
+    "hello".toInt()
+}
+result.isFailure == true
+result.error() == NumberFormatException
+```
+It'll catch all thrown `Throwable`s
+
+Because of using `Nothing` as representation of missing type, you can safely cast `Result` to any needed type:
+```
+fun loadUser(): Result<User, Throwable> {
+   val result: Result<User, Nothing> = Result.success(User())
+   return result // this works fine despite function requires Result<User, Throwable> return type
+}
+```
+
+### Getting result:
+There are few ways to get result from `Result` object.  
+Native are:
+```
+val success = Result.success("hello")
+success.getOrThrow() == "hello"
+
+val error = Result.error("critical error")
+error.errorOrThrow() == "critical error"
+```
+But be aware that these functions throw `IllegalStateException` if you try to get inappropriate result.  
+To prevent this you can use checks:  
+`result.isSuccess` and `result.isFailure`
+
+Besides that you can use another extensions to unwrap value:
+```
+val result: Result<String, Throwable> = ...
+
+val name = result.get() // safe, returns nullable
+val error = result.error() // safe, returns nullable
+
+val (name, error) = result // destructuring declaration, safe, nullable
+
+// these are safe and returns Result itself:
+result.onSuccess { name -> println(name) }
+    .onError { error -> println(error) }
+    .withSuccess { println(this) }
+    .withError { println(this) }
+    .getOrDefault { "hello" }
+    .errorOrDefault { IllegalStateException() }
+ 
+ result.getOrDefault("hello") // safe, returns default if error
+ result.errorOrDefault(IllegalStateException()) // safe, returns default if success
+
+```
+### Modifications:
+You can use such modifiers as:  
+`map`, `flatMap`, `mapError`, `flatMapError`, `swap`.
+
+## Other things:
+`doIfSuccess` and `doIfError` are called in appropriate cases, but without values.  
+Replacement of `if (result.isSuccess) { }` and `if (result.isFailure) { }`
+
+## How to add to your project:
+**Step 1.** Add the JitPack repository to your build file.  
+Add this in your module's build.gradle at the end of repositories:  
+```
+repositories {
+    maven { url 'https://jitpack.io' }
+}
+```
+**Step 2.** Add the dependency
+```
+dependencies {
+    implementation "com.github.green-nick:Result:{put latest version here}"
+}
+```
--- src/main/kotlin/com/greennick/result/ResultExt.kt
+++ src/main/kotlin/com/greennick/result/ResultExt.kt
@@ -2,25 +2,48 @@
 
 package com.greennick.result
 
+/**
+ * @return success value or null if absent.
+ */
 fun <T, E> Result<T, E>.get(): T? = when {
     isSuccess -> getOrThrow()
     else -> null
 }
 
+/**
+ * @return error value or null if absent.
+ */
 fun <T, E> Result<T, E>.error(): E? = when {
     isFailure -> errorOrThrow()
     else -> null
 }
 
+/**
+ * See [get].
+ * Used for destructuring.
+ */
 operator fun <T, E> Result<T, E>.component1(): T? = get()
 
+/**
+ * See [error].
+ * Used for destructuring.
+ */
 operator fun <T, E> Result<T, E>.component2(): E? = error()
 
+/**
+ * Replace success with error and otherwise.
+ *
+ * @return new Result with swapped types and values.
+ */
 fun <T, E> Result<T, E>.swap(): Result<E, T> = when {
     isSuccess -> Result.error(getOrThrow())
     else -> Result.success(errorOrThrow())
 }
 
+/**
+ * @param action - called with success value if presents.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
     if (isSuccess) {
         action(getOrThrow())
@@ -28,6 +51,10 @@ inline fun <T, E> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
     return this
 }
 
+/**
+ * @param action - called with error value if presents.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
     if (isFailure) {
         action(errorOrThrow())
@@ -35,46 +62,99 @@ inline fun <T, E> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
     return this
 }
 
+/**
+ * @param action - called with success value as receiver if presents.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.withSuccess(action: T.() -> Unit): Result<T, E> = onSuccess(action)
 
+/**
+ * @param action - called with error value as receiver if presents.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.withError(action: E.() -> Unit): Result<T, E> = onError(action)
 
+/**
+ * @param action - called in case of success result.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.doIfSuccess(action: () -> Unit): Result<T, E> = onSuccess { action() }
 
+/**
+ * @param action - called in case of failed result.
+ * @return Result itself.
+ */
 inline fun <T, E> Result<T, E>.doIfError(action: () -> Unit): Result<T, E> = onError { action() }
 
+/**
+ * @return Result's success value if presents.
+ * Call [defaultValue] block and return its result otherwise.
+ */
 inline fun <R, T : R> Result<T, Any?>.getOrDefault(defaultValue: () -> R): R = when {
     isFailure -> defaultValue()
     else -> getOrThrow()
 }
 
+/**
+ * @return Result's error value if presents.
+ * Call [defaultValue] block and return its result otherwise.
+ */
 inline fun <R, E : R> Result<Any?, E>.errorOrDefault(defaultValue: () -> R): R = when {
     isSuccess -> defaultValue()
     else -> errorOrThrow()
 }
 
+/**
+ * @return Result's success value if presents.
+ * Returns [defaultValue] otherwise.
+ */
 fun <R, T : R> Result<T, Any?>.getOrDefault(defaultValue: R): R =
     getOrDefault { defaultValue }
 
+/**
+ * @return Result's error value if presents.
+ * Returns [defaultValue] otherwise.
+ */
 fun <R, E : R> Result<Any?, E>.errorOrDefault(defaultValue: R): R =
     errorOrDefault { defaultValue }
 
+/**
+ * Maps inner success value with [transformer] block if presents to new Result.
+ * Returns casted origin Result otherwise.
+ */
 inline fun <T, E, R> Result<T, E>.flatMap(transformer: (T) -> Result<R, E>): Result<R, E> = when {
     isSuccess -> transformer(getOrThrow())
     else -> this as Result<R, E>
 }
 
+/**
+ * Maps inner error value with [transformer] block if presents to new Result.
+ * Returns casted origin Result otherwise.
+ */
 inline fun <T, E, R> Result<T, E>.flatMapError(transformer: (E) -> Result<T, R>): Result<T, R> = when {
     isSuccess -> this as Result<T, R>
     else -> transformer(errorOrThrow())
 }
 
+/**
+ * Maps inner success value with [transformer] block if presents and return new Result with it.
+ * Returns casted origin Result otherwise.
+ */
 inline fun <T, E, R> Result<T, E>.map(transformer: (T) -> R): Result<R, E> =
     flatMap { Result.success(transformer(it)) }
 
+/**
+ * Maps inner error value with [transformer] block if presents and return new Result with it.
+ * Returns casted origin Result otherwise.
+ */
 inline fun <T, E, R> Result<T, E>.mapError(transformer: (E) -> R): Result<T, R> =
     flatMapError { Result.error(transformer(it)) }
 
+/**
+ * Call [action] wrapped in try-catch block.
+ *
+ * @return Result with [action]`s value or with caught exception.
+ */
 inline fun <T> tryWithResult(action: () -> T): Result<T, Throwable> =
     try {
         Result.success(action())
